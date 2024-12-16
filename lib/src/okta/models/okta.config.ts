7ef65b7b0b93b609574af7378ba90eb14e896ad9
Brian Malinconico <brian.malinconico@terminus.com>
2024-12-16 09:22:11 -0500

    Force this

--- lib/src/okta/models/okta.config.ts
+++ lib/src/okta/models/okta.config.ts
@@ -26,5 +26,5 @@ export interface OktaConfig {
   testing?: TestingObject;
 }
 
-export const OKTA_CONFIG = new InjectionToken<OktaConfig>('okta.config.angular', { providedIn: 'root', factory: () => ({ oktaAuth: null }) });
+export const OKTA_CONFIG = new InjectionToken<OktaConfig>('okta.config.angular', { providedIn: 'root', factory: () => ({ oktaAuth: null } as any) });
 export const OKTA_AUTH = new InjectionToken<OktaAuth>('okta.auth', { providedIn: 'root', factory: () => new OktaAuth({} as any) });
